8674910d431810862505020a2cbda079a1f48c94
Vasvári Gergő <gergo.vasvari@gmail.com>
2024-02-09 02:42:30 +0100

    add norma calculation to trucks

--- models.py
+++ models.py
@@ -12,3 +12,20 @@ class Truck:
         self.distance_covered = 0
         self.fuel_tanked = 0
         self.cooling_time = 0
+        self.year = None
+        self.total_weight = None
+        self.self_weight = None
+        self.formula = None
+        self.performance = None
+        self.norma = None
+
+    def calc_norma(self):
+        if self.formula is not None:
+            formatted_formula = self.formula
+            formatted_formula.replace("Gm", str(self.total_weight))
+            formatted_formula.replace("Gs", str(self.self_weight))
+            formatted_formula.replace("N", str(self.performance))
+            formatted_formula.replace("0,", ".")
+            formatted_formula.replace(",", ".")
+            self.norma = round(eval(formatted_formula), 2)
+            
